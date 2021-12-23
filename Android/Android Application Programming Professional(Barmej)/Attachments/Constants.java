a8fa6f1817c12525ffe5488df9dee64ba9d917de
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-23 16:52:53 +0200

    Create Constants.java

---
+++ Android/Android Application Programming Professional(Barmej)/Attachments/Constants.java
@@ -0,0 +1,5 @@
+public static final String QUESTION_ANSWER = "question_answer";
+public static final String QUESTION_TEXT_EXTRA = "question_text_extra";
+public static final String APP_PREF = "app_pref";
+public static final String SHARE_TITLE = "question_text_extra";
+public static final String APP_LANG = "app_lang";
