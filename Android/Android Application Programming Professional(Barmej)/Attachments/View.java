df40384eb34e67fdabe4437cc62ba6be4c40e43d
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-26 08:24:54 +0200

    Create View.java

---
+++ Android/Android Application Programming Professional(Barmej)/Attachments/View.java
@@ -0,0 +1,10 @@
+private TextView mTextViewAnswer;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_answer);
+
+    mTextViewAnswer = findViewById(R.id.text_view_answer);
+
+}
