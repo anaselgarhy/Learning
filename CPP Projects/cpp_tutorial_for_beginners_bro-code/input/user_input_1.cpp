068ac9fa6e6ecf1ad6409ee7c0faa62dabbe15f3
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-04-02 05:37:22 +0200

    Add  the first 4 lessons from the cpp tutorial by bro code <3

---
+++ CPP Projects/cpp_tutorial_for_beginners_bro-code/input/user_input_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+	cout << "Please enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
+	return 0;
+}
---
+++ CPP Projects/cpp_tutorial_for_beginners_bro-code/variables/v_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace  std;
+
+int main() {
+	int year = 2022;
+    double salary = 1000.0;
+	char status = 'N';
+	string model = "BMW";
+	bool is_available = true;
+
+	cout << "Year: " << year << endl;
+	cout << "Salary: " << salary << endl;
+	cout << "Status: " << status << endl;
+	cout << "Model: " << model << endl;
+	cout << "Is available: " << is_available << endl;
+
+	return 0;
+}
