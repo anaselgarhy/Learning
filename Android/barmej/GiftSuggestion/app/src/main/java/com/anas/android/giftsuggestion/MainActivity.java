690103d5077d09563106c084837664af2740125e
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-06 05:17:44 +0200

    Init 🥰

---
+++ Android/barmej/GiftSuggestion/app/src/main/java/com/anas/android/giftsuggestion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anas.android.giftsuggestion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.anas.learning.android.android101.giftsuggestion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
